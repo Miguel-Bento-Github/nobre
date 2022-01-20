81e52403903c390370c044bc6d3c4c2ae523c738
alex_nobre <4aa@tutanota.com>
2022-01-20 18:57:37 +0000

    About working

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: "/about",
       name: "About",
-      component: () => import("@/views/About.vue"),
+      component: () => import("@/views/AboutView.vue"),
     },
   ],
 });
