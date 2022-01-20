b135734b1f23d1c92664c3bd39e213a119ecb6e3
alex_nobre <4aa@tutanota.com>
2022-01-20 18:43:23 +0000

    agora é que é

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
     },
     {
       path: "/about",
-      name: "about",
+      name: "About",
       component: () => import("@/views/About.vue"),
     },
   ],
---
+++ src/views/About.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Page created in colaboration with mr Robott</h1>
+</template>
